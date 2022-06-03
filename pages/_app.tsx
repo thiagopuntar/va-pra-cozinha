68502e774ba10d36636ae368f4b1166e2a1f272a
gdantasc <gabrieldcavalcante00@gmail.com>
2022-06-02 21:10:56 -0300

    refactor: change the themeProvider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../style/global";
+import  theme  from "../style/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,7 @@
 import type { NextPage } from "next";
-import { GlobalStyle } from "../style/global";
-import { ThemeProvider } from "styled-components";
-import { theme } from "../style/theme";
 
 const Home: NextPage = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <h1>Hello world!</h1>
-      <GlobalStyle />
-    </ThemeProvider>
-  );
+  return <h1>Hello world!</h1>;
 };
 
 export default Home;
